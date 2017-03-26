010411b35d77fe7973b742de52dd984a39a84e32
haukurb <haukzi@gmail.com>
2017-03-26 13:25:58 +0000

    sql added

--- databaseAPI.js
+++ databaseAPI.js
@@ -1,55 +1,134 @@
-// userId = a unique permanent identifier for a user, that our database creates
-// uid = String used as a unique permanent identifier for a user in firebase
-// instanceToken = String token associated with a single device and a single user, may change frequently
-// messageId = a unique identifier for this message (TODO: should we create our own or use the one created by firebase?)
+let sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Dtabase("./cdm.db");
 
+let CREATE_TABLE_USER = 
+            "CREATE TABLE IF NOT EXISTS users (" +
+            " id AUTOINCREMENT INTEGER PRIMARY KEY," + 
+            " firebase_uid TEXT ," + 
+            " username TEXT " + 
+            " email TEXT )"; 
 
-module.exports = {
-	// updates the database with the possibly new instanceToken, you can use the uid to identify the user and create a new user if she didn't already exist
-	// returns our own user id that the database created
-	registerUser : function (uid, instanceToken) {
-		
-	},
+let CREATE_TABLE_TOKENS = 
+            "CREATE TABLE IF NOT EXISTS tokens (" +
+            " id INTEGER PRIMARY KEY, " +
+            " instance_token TEXT )" +
 
-	// should do the same as registerUser except this is used if the user already exists // I think we should only use registerUser and the database deals with it
-	refreshUser : function (uid, instanceToken) {
+let CREATE_TABLE_MESSAGES = 
+            "CREATE TABLE IF NOT EXISTS messages (" +
+            " content TEXT, " +
+            " sender INTEGER, " + 
+            " receiver INTEGER, " +
+            " sent_time INTEGER )"; 
 
-	},
+let CREATE_TABLE_CONTACTS =
+            "CREATE TABLE IF NOT EXISTS contacts (" +
+            " user_id INTEGER PRIMARY KEY, " + 
+            " friend_id INTEGER"
+            " blocked BOOLEAN )";
 
-	// senderId = userId of the sender, receiverId = userId of the receiver, messageId = a unique identifier for this message (should we create our own or use the one created by firebase?)
-	// adds the message to the correct place
-	// returns an array of instanceTokens registered to the receiver
-	addMessage : function (senderId, receiverId, sentTime, messageId) {
+db.run(CREATE_TABLE_MESSAGES);
+db.run(CREATE_TABLE_CONTACTS);
+db.run(CREATE_TABLE_USER);
+db.run(CREATE_TABLE_TOKENS);
 
-	},
+function insertUser(uid, instanceToken, username, email) {
+	return new Promise(function(resolve, reject) {
+		let userSql = "INSERT INTO users (firebase_uid, email, username) VALUES (?, ?, ?)";
+		let tokenSql = "INSERT INTO tokens (id, token) values (?, ?)";
+		var userId;
+		db.serialize(function() {
+			db.run("BEGIN");
+			db.run(userSql, [uid, email, username], function(err) {
+				if(err) {
+					reject(err)
+				}
+				userId = this.lastID;
+				db.run(tokenSql, [userId, instanceToken], function(err) {
+					if(err) {
+						db.run("ROLLBACK");
+						reject(err);
+					}
+				});
+			});
+			db.run("COMMIT");
+		}
+	});
+}
 
-	// returns the user (must be at least their userId and username) that matches the searchString the "best" 
-	// (this method will be called after every input from the user, e.g. the user is searching for "tom" it will be called with "t", then "to", then "tom")
-	searchForContact : function (searchString) {
+function insertToken(userId, instanceToken) {
+	return new Promise(function(resolve, reject) {
+		let sql = "INSERT INTO tokens (id, instance_token) values (?, ?)";
+		db.run(sql, [userId, instanceToken], function(err) {
+			if(err) reject(err);
+			resolve(inserToken)
+		})
+	});
+}
 
-	},
+module.exports.insertOrUpdateUser = function(uid, instanceToken, username, email) {
+    let sql = "SELECT EXISTS ( SELECT id FROM users WHERE firebase_uid = ? LIMIT 1 )";
+    return new Promise(function(resolve, reject) {
+        db.get(sql, [uid], function(err, row) {
+        	if(err) reject(err);
+        	if (row === 0) 
+        		resolve(insertUser(uid, instanceToken, username, email));
+        	else {
+        		resolve(updateUser(uid, instanceToken, username, email));
+        	}
+        })
+    });  
+}
 
-	// adderId = userId of the adder, addedId = userId of the added
-	// adds a user (addedId) to another users (adderId) contacts
-	addContact : function (adderId, addedId) {
+module.exports.insertMessage = function(content, senderId, receiverId, sentTime) {
+	return new Promise(function(resolve, reject) {
+		let sql = "INSERT INTO messages (content, sender, receiver, sent_time)" + 
+					" VALUES (?, ?, ?, ?)";
+		db.run(sql, [content, senderId, receiverId, sentTime], function(err) {
+			if(err) reject(err);
+			resolve();
+		})
+	});
+}
 
-	},
+module.exports.searchForContact = function(searchString) {
+	return new Promise(function(resolve, reject) {
+		let sql = "SELECT id, username FROM users WHERE username = ?";
+		db.get(sql, searchString, function(err,row) {
+			if(err) reject(err);
+			resolve(row);
+		});
+	});
+}
 
-	// deleterId = userId of the deleter, deletedId = userId of the deleted
-	// removes a user (deletedId) from another users (deleterId) contacts
-	deleteContact : function (deleterId, deletedId) {
+module.exports.addContact = function(adderId, addedId) {
+	return new Promise(function(resolve, reject) {
+		let sql = "INSERT INTO contacts (user_id, friend_id, blocked) values (?, ?, false)";
+		db.run(sql, [adderId, addedId], function(err) {
+			if(err) reject(err);
+			resolve();
+		});
+	});
+}
 
-	},
+function setBlocked(blockerId, bockedId, blockState) {
+	return new Promise(function(resolve, reject) {
+		let sql = "UPDATE contacts SET blocked = ? WHERE user_id=? AND friend_id = ?";
+		db.run(sql, [blockerId, blockedId, blockedState], function(err) {
+			if(err) reject(err);
+			resolve();
+		})
+	});
+}
 
-	// blockerId = userId of the blocker, blockedId = userId of the blocked
-	// add a user (blockedId) to another users (blockerId) blocked "list"
-	blockContact : function (blockerId, blockedId) {
+module.exports.blockContact = function(blockerId, bockedId) { return setBLocked(blockerId, blockedId, false);};
+module.exports.unblockContact = function(blockerId, bockedId) { return setBLocked(blockerId, blockedId, true);};
 
-	},
-
-	// unblockerId = userId of the unblocker, unblockedId = userId of the unblocked
-	// remove a user (unblockedId) from another users (unblockerId) blocked "list"
-	unblockContact : function (unblockerId, unblockedId) {
-
-	}
+module.exports.deleteContact = function(deleterId, deletedId) {
+	return new Promise(function(resolve, reject) {
+		let sql = "DELETE FROM contacts WHERE user_id = ? AND friend_id = ?";
+		db.run(sql, [deleterId, deletedId], function(err) {
+			if(err) reject(err);
+			resolve();
+		})
+	});
 }
